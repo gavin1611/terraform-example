b2e1271c87d39472c2eacc47025dff3f0b14a11d
Gavin Lobo <gavlobo@deloitte.nl>
2021-02-25 15:19:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,30 @@ provider "aws" {
 }
 
 resource "aws_instance" "my-new-ec2" {
-  ami           = "ami-0526a2ea416570f68"
+  ami           = "ami-096f43ef67d75e998"
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my_subnet.id
+
 
   tags = {
     Name = "gavin-example-ec2"
+  }
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "tf-example-subnet"
   }
 }
